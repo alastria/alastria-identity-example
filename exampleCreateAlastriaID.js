75b82e9d1819ec0e1d431db0e8fafb8270587264
Víctor Nieves Sánchez <vnievess@gmail.com>
2019-10-08 12:26:51 +0200

    added "Data" comment

--- exampleCreateAlastriaID.js
+++ exampleCreateAlastriaID.js
@@ -10,8 +10,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider(myBlockchainServiceIp))
 console.log('VERSION: ', web3.version)
 
 console.log('\n ------ Example of prepare Alastria ID, addKey and createAlastrisID necessary to have an Alastria ID ------ \n')
-
-// Some fake data to test
+// Data
 const rawPublicKey = '03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479'
 
 let adminKeyStore = {"address":"6e3976aeaa3a59e4af51783cc46ee0ffabc5dc11","crypto":{"cipher":"aes-128-ctr","ciphertext":"463a0bc2146023ac4b85f4e3675c338facb0a09c4f83f5f067e2d36c87a0c35e","cipherparams":{"iv":"d731f9793e33b3574303a863c7e68520"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"876f3ca79af1ec9b77f181cbefc45a2f392cb8eb99fe8b3a19c79d62e12ed173"},"mac":"230bf3451a7057ae6cf77399e6530a88d60a8f27f4089cf0c07319f1bf9844b3"},"id":"9277b6ec-6c04-4356-9e1c-dee015f459c5","version":3};
@@ -35,6 +34,7 @@ try{
 }
 
 let subjectIdentity = new UserIdentity(web3, `0x${identityKeystore.address}`, subjectPrivateKey)
+// End data
 
 console.log('\n ------ Step one---> prepareAlastriaID inside a Promise ------ \n')
 let p1 = new Promise (async(resolve, reject) => {
