f0b08e5a5a1e52184b95152a58886a747921093f
Víctor Nieves Sánchez <vnievess@gmail.com>
2019-10-23 11:20:41 +0200

    removed hardcoded input argument

--- examplePresentations/4.updateSubjectPresentation.js
+++ examplePresentations/4.updateSubjectPresentation.js
@@ -11,7 +11,7 @@ let Web3 = require('web3')
 let myBlockchainServiceIp = configData.nodeURL
 const web3 = new Web3(new Web3.providers.HttpProvider(myBlockchainServiceIp))
 
-let updateSubjectPresentation = transactionFactory.presentationRegistry.updateSubjectPresentation(web3, presentationHash.psmhash, 2)
+let updateSubjectPresentation = transactionFactory.presentationRegistry.updateSubjectPresentation(web3, presentationHash.psmhash, configData.updateSubjectPresentationTo)
 console.log(updateSubjectPresentation)
 
 let keyData = fs.readFileSync('../keystore.json')
