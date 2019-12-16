ebf61d855373dbe7e8efc60fcbaa748cd565df0a
rober12 <robertgar1993@hotmail.com>
2019-12-16 12:04:01 +0100

    added example of addIdentityIssuer and deleteIdentityIssuer

--- exampleCreateAlastriaID/2.createSubjectAlastriaID.js
+++ exampleCreateAlastriaID/2.createSubjectAlastriaID.js
@@ -15,38 +15,50 @@ const web3 = new Web3(new Web3.providers.HttpProvider(myBlockchainServiceIp))
 
 console.log('\n ------ Example of prepare Alastria ID, addKey and createAlastrisID necessary to have an Alastria ID ------ \n')
 // Data
-const rawPublicKeyReceiver = configData.rawPublicKeyReceiver
+const rawPublicKey = configData.rawPublicKeySubject
 
-let adminKeyStore = keystoreData.adminKeystore
+// let adminKeyStore = keystoreData.adminKeystore
 
-let adminPrivateKey
+// let adminPrivateKey
+// try{
+// 	adminPrivateKey = keythereum.recover(keystoreData.addressPassword, adminKeyStore)
+// }catch(error){
+// 	console.log("ERROR: ", error)
+// }
+
+// let adminIdentity = new UserIdentity(web3, `0x${adminKeyStore.address}`, adminPrivateKey)
+
+let identityKeystore = keystoreData.identityKeystore
+
+
+let issuerKeystore = keystoreData.issuerKeystore
+
+let issuerPrivateKey
 try{
-	adminPrivateKey = keythereum.recover(keystoreData.addressPassword, adminKeyStore)
+	issuerPrivateKey = keythereum.recover(keystoreData.addressPassword, issuerKeystore)
 }catch(error){
 	console.log("ERROR: ", error)
 }
 
-let adminIdentity = new UserIdentity(web3, `0x${adminKeyStore.address}`, adminPrivateKey)
-
-let receiverKeystore = keystoreData.receiverKeystore
+let issuerIdentity = new UserIdentity(web3, `0x${issuerKeystore.address}`, issuerPrivateKey)
 
 let subjectPrivateKey
 try{
-	subjectPrivateKey = keythereum.recover(keystoreData.addressPassword, receiverKeystore)
+	subjectPrivateKey = keythereum.recover(keystoreData.addressPassword, identityKeystore)
 }catch(error){
 	console.log("ERROR: ", error)
 }
 
-let subjectIdentity = new UserIdentity(web3, `0x${receiverKeystore.address}`, subjectPrivateKey)
+let subjectIdentity = new UserIdentity(web3, `0x${identityKeystore.address}`, subjectPrivateKey)
 // End data
 
-function preparedAlastriaId()  {
-	let preparedId = transactionFactory.identityManager.prepareAlastriaID(web3, receiverKeystore.address)
+function preparedAlastriaId() {
+	let preparedId = transactionFactory.identityManager.prepareAlastriaID(web3, identityKeystore.address)
 	return preparedId
 }
 
 function createAlastriaId() {
-	let txCreateAlastriaID = transactionFactory.identityManager.createAlastriaIdentity(web3, rawPublicKeyReceiver)
+	let txCreateAlastriaID = transactionFactory.identityManager.createAlastriaIdentity(web3, rawPublicKey)
 	return txCreateAlastriaID
 }
 
@@ -55,7 +67,7 @@ async function main() {
 	let prepareResult = await preparedAlastriaId()
 	let createResult = await createAlastriaId()
 
-	let signedPreparedTransaction = await adminIdentity.getKnownTransaction(prepareResult)
+	let signedPreparedTransaction = await issuerIdentity.getKnownTransaction(prepareResult)
 	let signedCreateTransaction =	await subjectIdentity.getKnownTransaction(createResult)
 	web3.eth.sendSignedTransaction(signedPreparedTransaction)
 	.on('transactionHash', function (hash) {
@@ -71,11 +83,11 @@ async function main() {
 				console.log("RECEIPT: ", receipt)
 				web3.eth.call({
 					to: config.alastriaIdentityManager,				       
-					data: web3.eth.abi.encodeFunctionCall(config.contractsAbi['AlastriaIdentityManager']['identityKeys'], [receiverKeystore.address])
+					data: web3.eth.abi.encodeFunctionCall(config.contractsAbi['AlastriaIdentityManager']['identityKeys'], [identityKeystore.address])
 				})
 				.then (AlastriaIdentity => {
 					console.log(`alastriaProxyAddress: 0x${AlastriaIdentity.slice(26)}`)
-					configData.receiver = `0x${AlastriaIdentity.slice(26)}`
+					configData.subject = `0x${AlastriaIdentity.slice(26)}`
 					fs.writeFileSync('../configuration.json', JSON.stringify(configData))
 					let alastriaDID = tokensFactory.tokens.createDID('quor', AlastriaIdentity.slice(26));
 					console.log('the alastria DID is:', alastriaDID)
@@ -85,5 +97,6 @@ async function main() {
 	})
 	.on('error', console.error); // If a out of gas error, the second parameter is the receipt.
 }
+
 main()
 
--- exampleCreateAlastriaID/3.createReceiverAlastriaID.js
+++ exampleCreateAlastriaID/3.createReceiverAlastriaID.js
@@ -15,38 +15,52 @@ const web3 = new Web3(new Web3.providers.HttpProvider(myBlockchainServiceIp))
 
 console.log('\n ------ Example of prepare Alastria ID, addKey and createAlastrisID necessary to have an Alastria ID ------ \n')
 // Data
-const rawPublicKey = configData.rawPublicKeySubject
+const rawPublicKeyReceiver = configData.rawPublicKeyReceiver
 
-let adminKeyStore = keystoreData.adminKeystore
+// let adminKeyStore = keystoreData.adminKeystore
 
-let adminPrivateKey
-try{
-	adminPrivateKey = keythereum.recover(keystoreData.addressPassword, adminKeyStore)
-}catch(error){
-	console.log("ERROR: ", error)
-}
+// let adminPrivateKey
+// try{
+// 	adminPrivateKey = keythereum.recover(keystoreData.addressPassword, adminKeyStore)
+// }catch(error){
+// 	console.log("ERROR: ", error)
+// }
 
-let adminIdentity = new UserIdentity(web3, `0x${adminKeyStore.address}`, adminPrivateKey)
+// let adminIdentity = new UserIdentity(web3, `0x${adminKeyStore.address}`, adminPrivateKey)
 
 let identityKeystore = keystoreData.identityKeystore
 
-let subjectPrivateKey
+
+let issuerKeystore = keystoreData.issuerKeystore
+
+let issuerPrivateKey
 try{
-	subjectPrivateKey = keythereum.recover(keystoreData.addressPassword, identityKeystore)
+	issuerPrivateKey = keythereum.recover(keystoreData.addressPassword, issuerKeystore)
 }catch(error){
 	console.log("ERROR: ", error)
 }
 
-let subjectIdentity = new UserIdentity(web3, `0x${identityKeystore.address}`, subjectPrivateKey)
+let issuerIdentity = new UserIdentity(web3, `0x${issuerKeystore.address}`, issuerPrivateKey)
+
+let receiverKeystore = keystoreData.receiverKeystore
+
+let subjectPrivateKey
+try{
+	subjectPrivateKey = keythereum.recover(keystoreData.addressPassword, receiverKeystore)
+}catch(error){
+	console.log("ERROR: ", error)
+}
+
+let subjectIdentity = new UserIdentity(web3, `0x${receiverKeystore.address}`, subjectPrivateKey)
 // End data
 
-function preparedAlastriaId() {
-	let preparedId = transactionFactory.identityManager.prepareAlastriaID(web3, identityKeystore.address)
+function preparedAlastriaId()  {
+	let preparedId = transactionFactory.identityManager.prepareAlastriaID(web3, receiverKeystore.address)
 	return preparedId
 }
 
 function createAlastriaId() {
-	let txCreateAlastriaID = transactionFactory.identityManager.createAlastriaIdentity(web3, rawPublicKey)
+	let txCreateAlastriaID = transactionFactory.identityManager.createAlastriaIdentity(web3, rawPublicKeyReceiver)
 	return txCreateAlastriaID
 }
 
@@ -55,7 +69,7 @@ async function main() {
 	let prepareResult = await preparedAlastriaId()
 	let createResult = await createAlastriaId()
 
-	let signedPreparedTransaction = await adminIdentity.getKnownTransaction(prepareResult)
+	let signedPreparedTransaction = await issuerIdentity.getKnownTransaction(prepareResult)
 	let signedCreateTransaction =	await subjectIdentity.getKnownTransaction(createResult)
 	web3.eth.sendSignedTransaction(signedPreparedTransaction)
 	.on('transactionHash', function (hash) {
@@ -71,11 +85,11 @@ async function main() {
 				console.log("RECEIPT: ", receipt)
 				web3.eth.call({
 					to: config.alastriaIdentityManager,				       
-					data: web3.eth.abi.encodeFunctionCall(config.contractsAbi['AlastriaIdentityManager']['identityKeys'], [identityKeystore.address])
+					data: web3.eth.abi.encodeFunctionCall(config.contractsAbi['AlastriaIdentityManager']['identityKeys'], [receiverKeystore.address])
 				})
 				.then (AlastriaIdentity => {
 					console.log(`alastriaProxyAddress: 0x${AlastriaIdentity.slice(26)}`)
-					configData.subject = `0x${AlastriaIdentity.slice(26)}`
+					configData.receiver = `0x${AlastriaIdentity.slice(26)}`
 					fs.writeFileSync('../configuration.json', JSON.stringify(configData))
 					let alastriaDID = tokensFactory.tokens.createDID('quor', AlastriaIdentity.slice(26));
 					console.log('the alastria DID is:', alastriaDID)
@@ -85,6 +99,5 @@ async function main() {
 	})
 	.on('error', console.error); // If a out of gas error, the second parameter is the receipt.
 }
-
 main()
 
