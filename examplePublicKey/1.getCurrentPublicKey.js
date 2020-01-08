39b954bdce95563eb5db9dae93e4ec0ef2ff6987
rober12 <robertgar1993@hotmail.com>
2020-01-08 12:35:11 +0100

    fixed file getCurrentPublicKey whit the new keystore

--- examplePublicKey/1.getCurrentPublicKey.js
+++ examplePublicKey/1.getCurrentPublicKey.js
@@ -8,12 +8,12 @@ let configData = JSON.parse(rawdata)
 let myBlockchainServiceIp = configData.nodeURL
 const web3 = new Web3(new Web3.providers.HttpProvider(myBlockchainServiceIp))
 
-  if(configData.subject == undefined) {
+  if(configData.subject1 == undefined) {
     console.log('You must create an Alastria ID')
     process.exit()
   }
 
-let currentPubKey = transactionFactory.publicKeyRegistry.getCurrentPublicKey(web3, configData.subject)
+let currentPubKey = transactionFactory.publicKeyRegistry.getCurrentPublicKey(web3, configData.subject1)
 
 web3.eth.call(currentPubKey)
 .then(result => {
