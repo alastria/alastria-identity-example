f335069509709369fa3705092d5f56dc69f47832
Javier Aguirre <contacto@javaguirre.net>
2021-04-16 10:39:45 +0200

    Remove unused code isIdentityIssuer (#109)
    * Remove unused code on isIdentityIssuer
    
    * Keys check on configuration path

--- exampleFirstEntity/7.isIdentiyIssuer.js
+++ exampleFirstEntity/7.isIdentiyIssuer.js
@@ -1,52 +1,24 @@
-const { transactionFactory, UserIdentity } = require('alastria-identity-lib')
+const { transactionFactory } = require('alastria-identity-lib')
 const Web3 = require('web3')
 const fs = require('fs')
-const keythereum = require('keythereum')
 
-const rawdata = fs.readFileSync('../configuration.json')
+const CONFIG_NEEDED_KEYS = ['nodeURL', 'didEntity1']
+
+const rawdata = fs.readFileSync(process.env.CONFIGURATION_PATH || '../configuration.json')
 const configData = JSON.parse(rawdata)
 
-const keyDataAdmin = fs.readFileSync(
-  '../keystores/admin-6e3976aeaa3a59e4af51783cc46ee0ffabc5dc11.json'
-)
-const keystoreDataAdmin = JSON.parse(keyDataAdmin)
+const CONFIG_HAS_ALL_KEYS = CONFIG_NEEDED_KEYS.every(item => configData.hasOwnProperty(item))
+
+if (!CONFIG_HAS_ALL_KEYS) {
+  console.log(`You need the following keys in your config to run this example: ${CONFIG_NEEDED_KEYS.join(', ')}`)
+  process.exit(1)
+}
 
 // Init your blockchain provider
 const myBlockchainServiceIp = configData.nodeURL
 const web3 = new Web3(new Web3.providers.HttpProvider(myBlockchainServiceIp))
 
-const adminKeyStore = keystoreDataAdmin
-
-let adminPrivateKey
-try {
-  adminPrivateKey = keythereum.recover(
-    configData.addressPassword,
-    adminKeyStore
-  )
-} catch (error) {
-  console.log('ERROR: ', error)
-  process.exit(1)
-}
-
-const adminIdentity = new UserIdentity(
-  web3,
-  `0x${adminKeyStore.address}`,
-  adminPrivateKey
-)
-
-// Im not sure if this is needed
-async function unlockAccount() {
-  const unlockedAccount = await web3.eth.personal.unlockAccount(
-    adminIdentity.address,
-    configData.addressPassword,
-    500
-  )
-  console.log('Account unlocked:', unlockedAccount)
-  return unlockedAccount
-}
-
 async function main() {
-  unlockAccount()
   console.log('\n ------ Example of asking for isIdentityIssuer ------ \n')
   const isIssuer = await transactionFactory.identityManager.isIdentityIssuer(
     web3,
